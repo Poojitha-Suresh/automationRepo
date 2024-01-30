67a15a6b529a6eceee168358268cee34a50316d7
Poojitha-Suresh <poojitha.suresh@whitehatjr.com>
2024-01-30 14:36:37 +0530

    second commit

--- AutomationPractice/src/practice/secondJavaFile.java
+++ AutomationPractice/src/practice/secondJavaFile.java
@@ -1,12 +1,10 @@
 package practice;
 
-public class firstJavaFile {
+public class secondJavaFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Hello World");
-
+		System.out.println("Second Hello World");
 	}
 
 }
